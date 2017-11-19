f8d2ace3f9bfaaec6171c2d5055c8045c3230e5f
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-11-19 23:40:23 +0900

    共通で使いそうなクラスの作成

---
+++ SampleProject/Basic.swift
@@ -0,0 +1,11 @@
+//
+//  Basic.swift
+//  SampleProject
+//
+//  Created by 水野祥子 on 2017/11/19.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+let mainFame = UIScreen.main.bounds
