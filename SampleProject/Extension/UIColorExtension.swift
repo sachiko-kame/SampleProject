ec47ada817c32c1e4a77794afdf35bd053e121c0
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-11-19 23:50:44 +0900

    よく使いそうなExtensionクラスの作成

---
+++ SampleProject/Extension/UIColorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIColorExtension.swift
+//  SampleProject
+//
+//  Created by 水野祥子 on 2017/11/19.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func rgbColor(rgbValue: UInt) -> UIColor{
+        return UIColor(
+            red:   CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >>  8) / 255.0,
+            blue:  CGFloat( rgbValue & 0x0000FF)        / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    class var sample: UIColor {
+        return UIColor.rgbColor(rgbValue: 0x00ACED)
+    }
+    
+}
