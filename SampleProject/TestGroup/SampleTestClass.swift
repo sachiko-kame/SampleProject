10d9ecd3b914d2cda717f625ecc4f6b77c7a5956
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-12-30 22:28:47 +0900

    わかりやすいurlをコメントとして記述のみ

--- SampleProject/TestGroup/SampleTestClass.swift
+++ SampleProject/TestGroup/SampleTestClass.swift
@@ -8,7 +8,8 @@
 
 import XCTest
 @testable import SampleProject
-
+//https://qiita.com/hmhmsh/items/a0289263b8ed2e1f808f
+//https://qiita.com/shirochan/items/10271912289dc563cc36
 class SampleTestClass: XCTestCase {
     
     func testMySample() {
