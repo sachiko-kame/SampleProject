6080a6dc0ff5eb1e4474a1eb9af63e7391b21352
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-12-30 14:47:22 +0900

    受け取ったHTMLを表示する

--- SampleProject/ViewController/QiitaDetileViewController.swift
+++ SampleProject/ViewController/QiitaDetileViewController.swift
@@ -8,29 +8,46 @@
 
 import UIKit
 
-class QiitaDetileViewController: UIViewController {
-
-    var qiitaimagUrl:String?
-    var qiitaText:String?
-    var urlText:String?
+class QiitaDetileViewController: UIViewController{
+    
+    var qiitaItem:Qiita?
     @IBOutlet weak var qiitaImage: UIImageView!
     @IBOutlet weak var qiitaTextView: UITextView!
     @IBOutlet weak var urlTextView: UITextView!
     
+    
     convenience init(Item:Qiita) {
         self.init()
-        self.qiitaimagUrl = Item.profile_image_url
-        self.qiitaText = Item.title
-        self.urlText = Item.url
+        self.qiitaItem = Item
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        qiitaTextView.text = self.qiitaText
-        urlTextView.text = self.urlText
-        
-        let imgURL = URL(string: self.qiitaimagUrl!)!
+        sampleData.sample(request: QiitaDetileRequest(ID:(self.qiitaItem?.id)!)){ response in
+            switch response{
+            case .success(let response):
+                self.qiitaViewMake(response:response)
+                print("qiita詳細取得")
+            case .failure(.responseError(let responseError as SampleError)):
+                print("サーバーの通信による失敗\(responseError)")
+            case .failure(.connectionError(_)):
+                print("通信失敗")
+            case .failure(.requestError(_)):
+                print("とにかく失敗")
+            case .failure( _):
+                print("エラー")
+            }
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func qiitaViewMake(response:QiitaDetileitem){
+        let imgURL = URL(string: response.userimage)!
         let session = URLSession(configuration: .default)
         let download = session.dataTask(with: imgURL) { (data, response, error) in
             if (response as? HTTPURLResponse) != nil{
@@ -44,10 +61,27 @@ class QiitaDetileViewController: UIViewController {
         }
         session.invalidateAndCancel()
         download.resume()
-
+        
+        let htmlData = response.rendered.data(using: String.Encoding.utf8, allowLossyConversion:true)!
+        let attributedOptions : [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            NSAttributedString.DocumentReadingOptionKey(rawValue: NSAttributedString.DocumentAttributeKey.documentType.rawValue): NSAttributedString.DocumentType.html as AnyObject,
+            NSAttributedString.DocumentReadingOptionKey(rawValue: NSAttributedString.DocumentAttributeKey.characterEncoding.rawValue): String.Encoding.utf8.rawValue as AnyObject
+        ]
+        
+        // 文字列の変換処理
+        var attributedString:NSAttributedString?
+        do {
+            attributedString = try NSAttributedString(
+                data: htmlData,
+                options: attributedOptions,
+                documentAttributes: nil
+            )
+            qiitaTextView.attributedText = attributedString
+        } catch let e {
+            qiitaTextView.text = e.localizedDescription
+        }
+        
+        urlTextView.text = response.url
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
 }
